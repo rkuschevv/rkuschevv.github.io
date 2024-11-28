4bad2527e75f502c6109ed19ba07aa620f59f7d6
rkuschevv <roman_kusch@mail.ru>
2024-11-28 21:00:51 +0300

    DONE!!

--- app/main.py
+++ app/main.py
@@ -1,10 +1,6 @@
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
-from pydantic import BaseModel
-from fastapi.middleware.cors import CORSMiddleware
-from typing import Optional
-#from chatbot import handle_action
 
 app = FastAPI()
 
@@ -13,18 +9,6 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 app.mount("/images", StaticFiles(directory="images"), name="images")
 
 templates = Jinja2Templates(directory="templates")
-
-# class ChatRequest(BaseModel):
-#     act: str
-#     var1: Optional[str] = None
-#     var2: Optional[str] = None
-
 @app.get("/")
 async def read_root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
-
-# @app.post("/chat")
-# async def chat_endpoint(data: ChatRequest):
-#     result = handle_action(data.act, data.var1, data.var2)
-#     return result
-
